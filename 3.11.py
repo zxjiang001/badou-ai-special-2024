deca2c87529be09caaa2ef24b33910f138c28766
zhuyiguang <164116319+zhuyiguang@users.noreply.github.com>
2024-03-20 23:06:25 +0800

    Add files via upload
    test

---
+++ 3.11.py
@@ -0,0 +1,41 @@
+# a = int(input())
+# b = int(input())
+# c = a + b
+# print(type(a),type(b))
+# print('{}+{}={}'.format(a,b,c))
+
+# def fun(a, b, c):
+#     """
+#
+#     :param a:
+#     :param b:
+#     :param c:
+#     :return:
+#     """
+#     print(a * b * c)
+#
+# fun(2,1,3)
+
+# a = (1, 2, 3)
+# a = a[:1] + (int(input()),)+a[1:]
+# print(a)
+
+# i = dict(a=1, b=2, c=3)
+# # j = {'a': 1, 'b': 2, 'c': 3}
+# # # print(str(i['a'])+str(i['b']))
+# # # print(i.get('a'))
+# # i['d']=4
+# # print(i)
+# # i.pop('a')
+# # print(i)
+# for key, value in i.items():
+#     print(key, value)
+
+# my_set={1,2,True}
+# print(my_set)
+
+def test():
+    
+
+
+test()
